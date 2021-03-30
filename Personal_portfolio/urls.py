5c8fa7d6f68687e89c9f0fc9570e739d4b844afa
Pratik Shahi <shahipr0@gmail.com>
2021-03-30 16:08:22 +0530

    added home page

--- Personal_portfolio/urls.py
+++ Personal_portfolio/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from portfolio import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), path("",
+                                          views.home, name="homepage")
 ]
 
 # to make meadia visible /open on admin page
